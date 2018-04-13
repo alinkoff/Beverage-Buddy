e8677b057c8b5b070b8db14c25e3b8748b80dd20
Matthew Berman <mjberman024@gmail.com>
2018-04-12 23:02:07 -0400

    Adding the reverse string function

--- Beverage Buddy/ViewController.swift
+++ Beverage Buddy/ViewController.swift
@@ -16,8 +16,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print(message)
+        print(reverse(text: "Pineapple"))
     }
 
+    func reverse(text: String)-> String {
+        return String(text.reversed())
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
